616102b592c04aab23928d0057df780a5b31d49b
Selim Ellieh <selim@Selims-MBP.lan>
2023-11-22 14:51:18 +0200

    Added cost function

--- helpers.py
+++ helpers.py
@@ -18,6 +18,14 @@ def importTrainingData(file_name, m, n):
     return X_train, Y_train
 
 
+def z_score_normalization(X):
+     # Calculate the mean and standard deviation for each feature
+     means = np.mean(X, axis=0)
+     stds = np.std(X, axis = 0)
+     # Apply the normalization formula: (X - mean) / std
+     X_standardized = (X - means) / stds
+     print(means)
+     return X_standardized, means, stds
 
 def scatter(x, y, title, y_label, x_label):
     plt.scatter(x, y, marker='x', c='r')
@@ -30,7 +38,7 @@ def predictSingleLoop(x, w, b):
     '''
     Prediction for some features x and using weights w and bias b
     '''
-    n = x.shape(0)
+    n = x.shape[0]
     p = 0
     for i in range(n):
         p_i = x[i] * w[i]
@@ -38,9 +46,27 @@ def predictSingleLoop(x, w, b):
     p = p + b
     return p
 
-def predict(x, w, b):
+def predict(X, W, b):
         '''
     Prediction with vectorization for some features x and using weights w and bias b
     '''
-        p = np.dot(x,w) + b
+        
+        p = np.dot(X,W) + b
         return p
+
+
+def computeCost(X, y, W, b):
+     '''
+     Given X, Y, w and b. Computes J(w, b) of w and b.
+     '''
+     m = X.shape[0]
+     
+     j = 0.0
+     for i in range(m):
+
+          f_wb_i = predict(X[i], W, b)
+          j_i = pow(y[i] - f_wb_i, 2)
+          j = j + j_i
+     j = j / (2 * m)
+     return j
+
--- main.py
+++ main.py
@@ -16,10 +16,24 @@ def main():
     print("Number of training examples (m)", len(x_train))
     print(y_train)
     # scatter(x_train[:, 0], y_train, "Quality vs Acidity", "Quality of Wine", "Fixed acidity")
-    print(x_train[:, 0])
-    b_init = np.random.rand()
-    w_init = np.random.rand(N)
+    #print(x_train[:, 0])
+    # Normalize
+    x_train_normalized, means, stds = z_score_normalization(x_train)
+    print("Normalizing x-train....")
+    print("Now, mean =", np.mean(x_train_normalized))
+    print("Now, std =", np.std(x_train_normalized))
+    b_init = np.random.rand() * 0.01
+    w_init = np.random.rand(N) * 0.01
+ 
+    print("b_init value:", b_init)
+    print("w_init value:", w_init)
     
+    x_vec = x_train_normalized[0, :]
+    print("x_vec value:", x_vec)
+    f_wb = predictSingleLoop(x_vec, w_init, b_init)
+    print("prediction f_wb:", f_wb)
+
+    print("Thus, initialliy cost at optimal w is:", computeCost(x_train_normalized, y_train, w_init, b_init))
 
 
 

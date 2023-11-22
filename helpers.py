ac883c30ff3f80549e6bd518fa7ca1a4601eb66d
Selim Ellieh <selim@Selims-MBP.lan>
2023-11-22 15:34:44 +0200

    Added batch gradient descent and prediction

--- helpers.py
+++ helpers.py
@@ -1,11 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+import math
 
 np.set_printoptions(precision=2)  # reduced display precision on numpy arrays
 
 
 def importTrainingData(file_name, m, n):
+    '''Import the wine training data, with m elements and n features'''
     X_train = np.zeros((m, n))
     Y_train = np.zeros((m,))
     with open(file_name) as f:
@@ -19,15 +21,27 @@ def importTrainingData(file_name, m, n):
 
 
 def z_score_normalization(X):
+     '''
+     Z-score normalization on X (m * n)
+     '''
      # Calculate the mean and standard deviation for each feature
      means = np.mean(X, axis=0)
      stds = np.std(X, axis = 0)
      # Apply the normalization formula: (X - mean) / std
-     X_standardized = (X - means) / stds
-     print(means)
-     return X_standardized, means, stds
+     X_normalized = (X - means) / stds
+  
+     return X_normalized, means, stds
+
+def normalizeElement(X, means, stds):
+     '''
+     Normalize one element X, for prediction
+     '''
+     x_normalized = (X - means) / stds
+     return x_normalized
+     
 
 def scatter(x, y, title, y_label, x_label):
+    '''Scatter dots (x, y), as y in function of x'''
     plt.scatter(x, y, marker='x', c='r')
     plt.title(title)
     plt.ylabel(y_label)
@@ -70,3 +84,38 @@ def computeCost(X, y, W, b):
      j = j / (2 * m)
      return j
 
+def computeGradient(X, y, W, b):
+     '''
+     Compute Gradient dJ/dWj and dJ/db
+     '''
+     m,n = X.shape
+     # Array to hold dj/dw(j)
+     dj_dw = np.zeros((n,))
+     dj_db = 0
+     # Summation from i = 0 to m -1
+     for i in range(m):
+          e = predict(X[i], W, b) - y[i]
+          for j in range(n):
+            # e * x[i, j]
+            dj_dw[j] = dj_dw[j] + e * X[i, j]
+          dj_db = dj_db + e
+     dj_dw = dj_dw / m
+     dj_db = dj_db / m
+     return dj_dw, dj_db
+
+def gradientDescent(X, y, w_in, b_in , learning_rate, num_iters):
+     '''
+     Perform batch gradient descent `num_iters` times to learn w and b.
+     '''
+     w = w_in.copy()
+     b = b_in
+
+     for i in range(num_iters):
+          # Calculate gradient at each step
+          dj_dw, dj_db = computeGradient(X, y, w, b)
+          w = w - learning_rate * dj_dw
+          b = b - learning_rate * dj_db
+
+          if i % math.ceil(num_iters / 10) == 0:
+               print(f"Iteration {i:4d}: Cost {computeCost(X, y, w, b):8.2f}")
+     return w,b
--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def main():
     print("Normalizing x-train....")
     print("Now, mean =", np.mean(x_train_normalized))
     print("Now, std =", np.std(x_train_normalized))
-    b_init = np.random.rand() * 0.01
-    w_init = np.random.rand(N) * 0.01
+    b_init = 0
+    w_init = np.zeros((N, ))
  
     print("b_init value:", b_init)
     print("w_init value:", w_init)
@@ -33,9 +33,36 @@ def main():
     f_wb = predictSingleLoop(x_vec, w_init, b_init)
     print("prediction f_wb:", f_wb)
 
-    print("Thus, initialliy cost at optimal w is:", computeCost(x_train_normalized, y_train, w_init, b_init))
-
-
+    print("Thus, intially cost at w is:", computeCost(x_train_normalized, y_train, w_init, b_init))
+    iterations = 3000
+    alpha = 5.0e-7
+    print(f"Performing batch gradient descent with iterations = {iterations} and learning rate = {alpha} ")
+    w_optimal, b_optimal = gradientDescent(x_train_normalized, y_train, w_init, b_init, 0.01, iterations)
+    print(f"Optimal w found by batch gradient descent is {w_optimal}!")
+    print(f"Optimal b found by batch gradient descent is {b_optimal}!")
+    print("Recall, x_vec value:", x_vec)
+    f_wb = predictSingleLoop(x_vec, w_optimal, b_optimal)
+    print("prediction f_wb using optimal w and b:", f_wb)
+    print("Try to predict this new wine: ")
+    '''
+    Predict wine with characteristics:
+    - Fixed Acidity: 8.5
+    - Volatile Acidity: 0.45
+    - Citric Acid: 0.3
+    - Residual Sugar: 2.5
+    - Chlorides: 0.08
+    - Free Sulfur Dioxide: 20
+    - Total Sulfur Dioxide: 75
+    - Density: 0.9968
+    - pH: 3.4
+    - Sulphates: 0.65
+    - Alcohol: 10.5
+    '''
+    x_pred = np.array([8.5,0.45,0.3,2.5,0.08,20,75,0.9968,3.4,0.65,10.5])
+    print("Predicing x_pred =", x_pred)
+    x_pred_normalized =normalizeElement(x_pred, means, stds)
+    f_wb = predictSingleLoop(x_pred_normalized, w_optimal, b_optimal)
+    print("prediction f_wb using optimal w and b:", f_wb)
 
 
 if __name__ == "__main__":
